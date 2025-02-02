0bbb978e22a81eadac267bf3cfbc2c5ed3ccfe9d
ron131977 <ronalddsouza888@gmail.com>
2025-02-02 15:07:04 +0530

    UPDATE  SITEMAP

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -56,44 +56,46 @@ async function generateSitemap() {
   const sitemapEntries = await sitemap()
 
   const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml"
-        xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-${sitemapEntries
-  .map(
-    (entry) => `
-  <url>
-    <loc>${entry.url}</loc>
-    <lastmod>${entry.lastModified.toISOString()}</lastmod>
-    <changefreq>${entry.changeFrequency}</changefreq>
-    <priority>${entry.priority}</priority>
-    ${
-      entry.images
-        ? entry.images
-            .map(
-              (image) => `
-      <image:image>
-        <image:loc>${image.loc}</image:loc>
-        <image:title>${image.title}</image:title>
-      </image:image>`
-            )
-            .join("")
-        : ""
-    }
-  </url>
-  `
-  )
-  .join("")}
-</urlset>`
-
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+          xmlns:xhtml="http://www.w3.org/1999/xhtml"
+          xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+          xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+          xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  ${sitemapEntries
+    .map(
+      (entry) => {
+        const sanitizedUrl = entry.url.replace(/<\/?script[^>]*>/g, "");  // Remove script tags if any
+        return `
+    <url>
+      <loc>${sanitizedUrl}</loc>
+      <lastmod>${entry.lastModified.toISOString()}</lastmod>
+      <changefreq>${entry.changeFrequency}</changefreq>
+      <priority>${entry.priority}</priority>
+      ${
+        entry.images
+          ? entry.images
+              .map(
+                (image) => `
+        <image:image>
+          <image:loc>${image.loc}</image:loc>
+          <image:title>${image.title}</image:title>
+        </image:image>`
+              )
+              .join("")
+          : ""
+      }
+    </url>
+    `;
+    })
+    .join("")}
+  </urlset>`;
+  
   // Format XML using Prettier
   const formattedXml = await prettier.format(xmlContent, {
     parser: "xml",
     // Explicitly preserve the first line with the XML declaration
-    printWidth: 80,
+    printWidth: 90,
     proseWrap: "always",
   })
 
